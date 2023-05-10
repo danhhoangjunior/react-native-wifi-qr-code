61224f8c64107d5f7f77f7e77a91fe76873c4088
c-dragon <phamhoang.ligosoft@gmail.com>
2023-05-11 01:29:00 +0700

    init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,130 @@
-import { StyleSheet } from 'react-native';
+import {
+    Button,
+    StyleSheet,
+    TextInput,
+    Text,
+    Pressable,
+    useColorScheme,
+    PermissionsAndroid,
+    ToastAndroid
+} from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import {View} from '../../components/Themed';
+import WifiManager from "react-native-wifi-reborn";
+import {useEffect, useState} from "react";
+import * as Location from 'expo-location'
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Colors from "../../constants/Colors";
+import {Link} from "expo-router";
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+    const colorScheme = useColorScheme();
+
+    const [ssid, setSsid] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+
+    useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        };
+
+        getBarCodeScannerPermissions();
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            const {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.log('Permission to access location was denied');
+                return;
+            }
+            const location = await Location.getCurrentPositionAsync({});
+            console.log('Location permission granted', location);
+        })();
+
+    }, []);
+
+    const connectWifi = async () => {
+        try {
+            WifiManager.connectToProtectedSSID(ssid, password, false)
+                .then(
+                    () => {
+                        console.log("Connected successfully!");
+                    },
+                    (err) => {
+                        console.log("Connection failed!", err);
+                    }
+                );
+        } catch (error) {
+            console.log('Connection failed!', {error});
+        }
+    }
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    return (
+        <View style={{flex: 1}}>
+            <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            />
+
+            <View style={styles.container}>
+                <TextInput style={styles.input} placeholder={'ssid'} onChangeText={setSsid}/>
+                <TextInput style={styles.input} placeholder={'password'} onChangeText={setPassword}/>
+                <Button
+                    onPress={connectWifi}
+                    title='Connect to Wifi'
+                    color='#841584'
+                />
+
+                <Link href="/modal" asChild>
+                    <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)}/>
+                </Link>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
+    input: {
+        height: 40,
+        width: '80%',
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: 'gray',
+        textAlign: 'center',
+        marginBottom: 12
+    }
 });
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,17 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import {Platform, StyleSheet} from 'react-native';
 import { Text, View } from '../components/Themed';
+import {BarCodeScanner} from "expo-barcode-scanner";
+import {useState} from "react";
+import WifiManager from "react-native-wifi-reborn";
+import {useNavigation} from "@react-navigation/native";
 
 export default function ModalScreen() {
+
+  const [scanned, setScanned] = useState(false);
+  const navigation = useNavigation()
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    connectWifi(data);
+  };
+
+  const connectWifi = async (data) => {
+    try {
+      const splits = data.split(";")
+      const sSsid = splits.find(v => v.includes("S:"))
+      const sPassword = splits.find(v => v.includes("P:"))
+      const myssid = sSsid.substring(sSsid.lastIndexOf("S:") + 2)
+      const mypassword = sPassword.substring(sSsid.lastIndexOf("P:") + 3)
+      const sType = splits.find(v => v.includes("T:"))
+      const myType = sType.substring(sType.lastIndexOf("T:") + 2)
+      console.log({ myssid, mypassword, myType })
+      //myType == "WEP" ? true : false
+
+      WifiManager.connectToProtectedSSID(myssid.toString(), mypassword.toString(), myType == "WEP" ? true : false)
+          .then(
+              () => {
+                console.log("Connected successfully!");
+                navigation.goBack()
+              },
+              (err) => {
+                console.log("Connection failed!", err);
+              }
+          );
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+      />
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
